5314429337fa970cc88f239399f900ee15a71cf1
Sarvadnya Chavhan <sarvadnyachavhan07@outlook.com>
2024-07-30 21:52:35 +0530

    added caching

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
+const NodeCache = require('node-cache');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,10 +9,21 @@ const port = process.env.PORT || 4000;
 // Use the CORS middleware
 app.use(cors());
 
+// Create a cache instance with a TTL (time-to-live) of 1 hour (3600 seconds)
+const screenshotCache = new NodeCache({ stdTTL: 3600 });
+
 app.get('/screenshot', async (req, res) => {
   const { url } = req.query;
   if (!url) {
     return res.status(400).send('URL is required');
+  }
+
+  // Check if the screenshot is already cached
+  const cachedScreenshot = screenshotCache.get(url);
+  if (cachedScreenshot) {
+    console.log(`Cache hit for URL: ${url}`);
+    res.set('Content-Type', 'image/png');
+    return res.send(cachedScreenshot);
   }
 
   let browser;
@@ -24,6 +36,9 @@ app.get('/screenshot', async (req, res) => {
     console.log("Screenshot is being captured of url: ", url);
     const screenshot = await page.screenshot();
     await browser.close();
+
+    // Store the screenshot in the cache
+    screenshotCache.set(url, screenshot);
 
     res.set('Content-Type', 'image/png');
     res.send(screenshot);
